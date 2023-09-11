b363ba885e2b1b6510ded607b6e48c786945b655
unknown <arifhasan.neon@gmail.com>
2023-09-11 14:34:47 +0600

    [done] added express mongoose cors

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from 'express';
+import cors from "cors";
+const app : Application = express();
+const port = 3000;
+
+
+app.use(cors())
+
+//parser
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+
+//Testing API
+app.get('/', (req:Request, res:Response) => {
+  res.send('working successfully');
+})
+
+
+export default app;
